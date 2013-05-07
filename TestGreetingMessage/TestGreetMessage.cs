2915034d332fe9c94547c076817fef4a55a84704
LIU Weishan <wsliu@thoughtworks.com>
2013-05-07 10:27:18 +0800

    basic feature

---
+++ TestGreetingMessage/TestGreetMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using GreetingMessage;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestGreetingMessage
+{
+    [TestClass]
+    public class TestGreetMessage
+    {
+        [TestMethod]
+        public void should_return_hello_as_default_greeting_message()
+        {
+            var message = new SimpleGreeting();
+            Assert.AreEqual("Hello!", message.GetMessage());
+        }
+    }
+}
--- demo/Form1.Designer.cs
+++ demo/Form1.Designer.cs
@@ -28,12 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(30, 91);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(181, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "What I want to say:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
     }
 }
 
